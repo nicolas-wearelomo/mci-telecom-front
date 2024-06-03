53cb8ba887a5adafe9e9b1cd23f2c896fe836e32
Nicolas Asef <nicolasasef1@gmail.com>
2024-06-03 15:15:29 -0300

    changes

--- src/app/akslasdaasd/kajsdadadas/page.tsx
+++ src/app/akslasdaasd/kajsdadadas/page.tsx
@@ -1,70 +1,14 @@
-// import Head from "next/head";
-// import ShareButton from "./component/ShareButton";
-
-// const BlogPost = () => {
-//   const post = {
-//     id: 1,
-//     title: "test de prueba",
-//     summary: "Summary",
-//     image: "https://ammper-lomo-images.s3.us-east-2.amazonaws.com/imagenes/alcance3.png",
-//   };
-//   return (
-//     <>
-//       <Head>
-//         <title>{post.title}</title>
-//         <meta property="og:title" content={post.title} />
-//         <meta property="og:description" content={post.summary} />
-//         <meta property="og:image" content={post.image} />
-//         <meta
-//           property="og:url"
-//           content={`http://localhost:3000/akslasdaasd/kajsdadadas`}
-//           // content={`http://ec2-3-17-135-121.us-east-2.compute.amazonaws.com/akslasdaasd/kajsdadadas`}
-//         />
-//         <meta name="twitter:card" content="summary_large_image" />
-//         <meta name="twitter:title" content={post.title} />
-//         <meta name="twitter:description" content={post.summary} />
-//         <meta name="twitter:image" content={post.image} />
-//       </Head>
-//       <div>
-//         <h1>{post.title}</h1>
-//         <p>{post.summary}</p>
-//         <img src={post.image} alt={post.title} />
-//         <ShareButton
-//           platform="facebook"
-//           url={`http://localhost:3000akslasdaasd/kajsdadadas`}
-//           // url={`http://ec2-3-17-135-121.us-east-2.compute.amazonaws.comakslasdaasd/kajsdadadas`}
-//           title={post.title}
-//           image={post.image}
-//         />
-//         <ShareButton
-//           platform="linkedin"
-//           url={`http://localhost:3000akslasdaasd/kajsdadadas`}
-//           // url={`http://ec2-3-17-135-121.us-east-2.compute.amazonaws.comakslasdaasd/kajsdadadas`}
-//           title={post.title}
-//           image={post.image}
-//         />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default BlogPost;
-
-import { Metadata } from "next";
+import Head from "next/head";
 import ShareButton from "./component/ShareButton";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "test de prueba",
   description: "Summary",
   openGraph: {
     title: "test de prueba",
     description: "Summary",
-    images: [
-      {
-        url: "https://ammper-lomo-images.s3.us-east-2.amazonaws.com/imagenes/alcance3.png",
-        alt: "test de prueba",
-      },
-    ],
+    image: "https://ammper-lomo-images.s3.us-east-2.amazonaws.com/imagenes/alcance3.png",
+
     url: "http://ec2-3-17-135-121.us-east-2.compute.amazonaws.com/akslasdaasd/kajsdadadas",
     type: "article",
   },
@@ -88,11 +32,46 @@ const BlogPost = () => {
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.summary}</p>
-      <img src={post.image} alt={post.title} />
-      <ShareButton platform="facebook" url={pageUrl} title={post.title} image={post.image} />
-      <ShareButton platform="linkedin" url={pageUrl} title={post.title} image={post.image} />
+      <Head>
+        <title>{post.title}</title>
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.summary} />
+        <meta property="og:image" content={post.image} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:type" content="article" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={post.title} />
+        <meta name="twitter:description" content={post.summary} />
+        <meta name="twitter:image" content={post.image} />
+      </Head>
+      <div>
+        <h1>{post.title}</h1>
+        <p>{post.summary}</p>
+        <img src={post.image} alt={post.title} />
+
+        {/* Facebook SDK */}
+        <div id="fb-root"></div>
+        <script
+          async
+          defer
+          // crossorigin="anonymous"
+          src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v20.0"
+          nonce="CdHdphvi"
+        ></script>
+
+        {/* Botón de compartir en Facebook */}
+        <div className="fb-share-button" data-href={pageUrl} data-layout="button" data-size="large">
+          <a
+            target="_blank"
+            href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(pageUrl)}&src=sdkpreparse`}
+            className="fb-xfbml-parse-ignore"
+          >
+            Compartir
+          </a>
+        </div>
+
+        <ShareButton platform="linkedin" url={pageUrl} title={post.title} image={post.image} />
+      </div>
     </div>
   );
 };
