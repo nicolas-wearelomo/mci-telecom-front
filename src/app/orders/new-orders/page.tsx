93f1027766f7d34d46e8bf04ac4ec8a56022abb6
Nicolas Asef <nicolasasef1@gmail.com>
2024-07-04 12:27:49 -0300

    fix

--- src/app/orders/new-orders/page.tsx
+++ src/app/orders/new-orders/page.tsx
@@ -1,18 +1,6 @@
-"use client";
 import NewOrderContainer from "@/containers/Orders/NewOrderContainer";
-import { RootState } from "@/redux/types";
-import useGetInfoPlan from "@/services/orders/useGetInfoPlan";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
 
 const page = () => {
-  const { currentUser } = useSelector((state: RootState) => state.auth);
-
-  const { callback, data, loading } = useGetInfoPlan();
-
-  useEffect(() => {
-    callback({ company: currentUser?.company });
-  }, [currentUser]);
   return <NewOrderContainer />;
 };
 
